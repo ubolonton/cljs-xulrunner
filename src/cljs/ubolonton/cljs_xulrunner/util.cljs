191128f0ca03300bf3c4c8fa8ff2c76d18d5e3d8
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-08-14 21:46:00 +0700

    some more util functions

--- src/cljs/ubolonton/cljs_xulrunner/util.cljs
+++ src/cljs/ubolonton/cljs_xulrunner/util.cljs
@@ -1,10 +1,44 @@
-(ns ubolonton.cljs-xulrunner.util)
+(ns ubolonton.cljs-xulrunner.util
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+  (:require [cljs.core.async :refer [<! >! put! timeout chan]]))
 
-(defn post [url data callback]
-  (let [request (XMLHttpRequest.)]
+(def Cc (.-classes js/Components))
+
+(def Ci (.-interfaces js/Components))
+
+(def Cu (.-utils js/Components))
+
+(defn class [class-id]
+  (aget Cc (str class-id)))
+
+(defn interface [interface-id]
+  (aget Ci (str interface-id)))
+
+(defn as [obj interface-id]
+  (.QueryInterface obj (interface interface-id)))
+
+(defn create [class-id interface-id]
+  (.createInstance (class class-id) (interface interface-id)))
+
+(defn xml-http-request []
+  (-> (create "@mozilla.org/xmlextras/xmlhttprequest;1" "nsIXMLHttpRequest")
+      (as "nsIJSXMLHttpRequest")
+      (as "nsIDOMEventTarget")))
+
+(defn post [url data]
+  (let [request (xml-http-request)
+        result (chan 1)]
     (doto request
       (aset "onload" (fn [event]
-                       (callback (.-responseText request))))
+                       (put! result (.-responseText request))))
       (aset "timeout" 50000)
       (.open "POST" url true)
-      (.send data))))
+      (.send data))
+    result))
+
+;;; Temp hack while piggy-backing on MozRepl for development Not
+;;; simply js/repl.print, because it's a method (using this), not a
+;;; function.
+(set-print-fn!
+ (fn [str]
+   (js/repl.print str)))
