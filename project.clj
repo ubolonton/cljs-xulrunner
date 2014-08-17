f904c04d088efa4f688fd7388e30751f276b6600
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-08-17 21:47:56 +0700

    clean up project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.ubolonton/xulrunner "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Tools to bring Clojurescript to XULRunner"
+  :url "https://github.com/ubolonton/cljs-xulrunner"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -10,11 +10,7 @@
 
   :source-paths ["src/clj"]
 
-  :plugins [[lein-cljsbuild "1.0.0"]
-            ;; [com.cemerick/austin "0.1.3"]
-            ]
-
-  ;; :repl-options {:init-ns ubolonton.xulrunner}
+  :plugins [[lein-cljsbuild "1.0.0"]]
 
   :profiles {:dev {:repl-options {:init-ns ubolonton.xulrunner}
                    :plugins [[com.cemerick/austin "0.1.3"]]}}
@@ -26,15 +22,4 @@
      :compiler {:optimizations :none
                 :pretty-print true
                 :output-dir "build"
-                :output-to "build/deps.js"}}
-    ;; {:id "simple"
-    ;;  :source-paths ["src/cljs"]
-    ;;  :compiler {:optimizations :simple
-    ;;             :output-dir "build-simple"
-    ;;             :output-to "build-simple/main.js"}}
-    ;; {:id "advanced"
-    ;;  :source-paths ["src/cljs"]
-    ;;  :compiler {:optimizations :advanced
-    ;;             :output-dir "build-advanced"
-    ;;             :output-to "build-advanced/main.js"}}
-    ]})
+                :output-to "build/deps.js"}}]})
