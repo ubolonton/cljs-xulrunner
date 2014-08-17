f5a8b658c84a6fe6ec3c1ea804218cf9979b4c45
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2014-08-17 21:44:09 +0700

    remove dependency on my specifc conkeror's config

--- src/js/run.js
+++ src/js/run.js
@@ -5,34 +5,32 @@ let (
   bootstrapModule = "file:///home/ubolonton/Programming/projects/cljs-xulrunner/src/js/bootstrap.js",
   bootstrap = {}
 ) {
+  let window = Components.classes["@mozilla.org/appshell/appShellService;1"]
+        .getService(Components.interfaces.nsIAppShellService)
+        .hiddenDOMWindow.wrappedJSObject;
   Services.scriptloader.loadSubScript(bootstrapModule, bootstrap);
   let env = bootstrap.main(base);
-
-  // Piggyng-backing on ubolonton's specific conkeror's function. This is
-  // important to give the env some globals clojure.browser namespaces
-  // expect. XXX: Use standard XULRunner stuff instead
-  let w = makeI().window;
-  env.Element = w.Element;
-  env.window = w;
-  env.document = w.document;
-  env.setTimeout = w.setTimeout;
-
+  // Manually set some important globals. TODO: Allow reusing an
+  // existing context instead of always creating an almost-empty one
+  // (see bootstrap/goog_make_env)
+  env.Element = window.Element;
+  env.window = window;
+  env.document = window.document;
+  env.setTimeout = window.setTimeout;
   // MozREPL piggy-backing. TODO: Use own's REPL
   env.repl = repl;
-
+  // Enter the context
   repl.enter(env);
 };
 
-// Try these *after* running the above
+// Run "lein repl", then run ubolonton.xulrunner.run
 
+// Run these in MozREPL *after* running the above, replace the url
+// with the one printed by ubolonton.xulrunner.run (minus the "start"
+// part, that's for nrepl)
 goog.require("ubolonton.xulrunner.repl");
+ubolonton.xulrunner.repl.connect("http://localhost:38746/7864/repl");
 
-cljs.core.println("?");
-
-// ubolonton.xulrunner.util
-
-
-// clojure.browser.repl.connect("http://localhost:40208/9405");
-
-
-// env = loadScript("file:///home/ubolonton/Programming/projects/cljs-xulrunner/src/js/bootstrap.js").main()
+// If you use a nrepl client (e.g. connecting to "lein repl" with
+// cider, turn off pretty printing, as clojure.pprint has not been
+// implemented for Clojurescript
